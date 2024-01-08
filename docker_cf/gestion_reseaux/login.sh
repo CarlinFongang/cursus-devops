0a86cae3dc47d917af3b81dba17c7d1495d6df8f
Carlin FONGANG <45420769+CarlinFongang@users.noreply.github.com>
2024-01-08 15:09:16 +0100

    création d'un main pour l'exécution des fichiers de script

---
+++ docker_cf/gestion_reseaux/login.sh
@@ -0,0 +1,3 @@
+#lancement du bash interactif sous ubuntu1
+docker exec -it ubuntu1 /bin/bash
+
---
+++ docker_cf/gestion_reseaux/main.sh
@@ -0,0 +1,11 @@
+#script de configuration du réseau
+sh config_rx.sh
+
+#script de lancement conteneur 1
+sh vm-1.sh
+
+#script de lancement conteneur 2
+sh vm-2.sh
+
+#connexion à la machine 1
+sh login.sh
