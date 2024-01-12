20df1cd194231dc346130b5a2068ae016f8bc523
Carlfg <fongangcarlin@gmail.com>
2024-01-12 23:09:29 +0100

    Mise à jour du Docker file et fixation des bugs

---
+++ docker_cf/projet_ls_pozos/build_api_img.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#build image
+docker build -t webapp:v1.0 .
+
+#deploy container
+docker run -it --name webapi1 -d -p 5000:80 webapi:v1.1 /bin/bash
+
+#test ping 
+curl -u toto:python -X GET http://localhost:8000/pozos/api/v1.0/get_student_ages
+
+#push images
